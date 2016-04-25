0254a388b1fb0af11339d19e79eb5863ca5a7b26
Kevin <kevin@Kevins-MBP.gateway>
2016-04-25 15:31:02 +1000

    Initial commit.

---
+++ src/components/app.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Component } from 'react';
+import GameBoard from './game_board';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <h3>React simple starter</h3>
+        <GameBoard />
+      </div>
+    );
+  }
+}
---
+++ src/components/game_board.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Component } from 'react';
+
+export default class GameBoard extends Component {
+  render() {
+    return (
+      <div>
+        GameBoard Goes here
+      </div>
+    );
+  }
+}
